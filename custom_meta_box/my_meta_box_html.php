73c909b5c12ef1e8f8fdd1844bfe2555cb477771
Sjors Roelofs <sjors.roelofs@gmail.com>
2014-10-16 18:54:21 +0200

    Added text field type to inputs

--- custom_meta_box/my_meta_box_html.php
+++ custom_meta_box/my_meta_box_html.php
@@ -1,5 +1,5 @@
 <h4>Value 1</h4>
-<input name="value-1" value="<?php echo $metaBoxContent['value-1']; ?>" />
+<input type="text" name="value-1" value="<?php echo $metaBoxContent['value-1']; ?>" />
 
 <h4>Value 2</h4>
-<input name="value-2" value="<?php echo $metaBoxContent['value-2']; ?>" />
+<input type="text" name="value-2" value="<?php echo $metaBoxContent['value-2']; ?>" />
